89649a4b3dbce2b14e04524f8b82e6238e3a0717
shumskiy <shumskiparents@gmail.com>
2026-05-08 06:57:39 +0200

    error

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,11 +1,13 @@
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  const isHttpError = err instanceof createHttpError.HttpError;
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
 
-  const status = isHttpError ? err.status : 500;
-
-  res.status(status).json({
-    message: err.message || 'Internal Server Error',
+  res.status(500).json({
+    message: 'Something went wrong',
   });
 };
